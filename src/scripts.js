139732d4fb516e8c8a4197ac7db5e0ca9356f7f2
Antonio Molinari <molinari@incode.it>
2023-04-18 23:47:42 +0200

    do not auto-open modal if modal has alredy been triggered

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,20 @@
 
+let formTriggered = false;
+
 function setup(){
   const modalForm = new bootstrap.Modal(document.getElementById('modalForm'), {
     keyboard: false
   })
-  setInterval(() => {
-    modalForm.show();
+  
+  Array.from(document.getElementsByClassName('triggersModal')).forEach((el) => {
+    el.addEventListener("click", () => {
+      console.log("triggersModal")
+      formTriggered = true;
+    })
+  })
+
+  setTimeout(() => {
+    if(!formTriggered) modalForm.show();
   }, 80*1000);
 }
 
